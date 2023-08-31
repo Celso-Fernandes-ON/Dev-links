29720e4cc14fd1b8eba96235a701f4754f50cbfa
Celso Fernandes <56808157+Celso-Fernandes-ON@users.noreply.github.com>
2023-08-31 20:48:48 -0300

    :finish:

--- script.js
+++ script.js
@@ -2,13 +2,4 @@ var back = 0;
 function toggleMode(){
     const html = document.documentElement
         html.classList.toggle('light')
-    // if (back > 2){
-    //     const html = document.documentElement
-    //     html.classList.toggle('apocalipse')
-    // }
-    // else{
-    //     const html = document.documentElement
-    //     html.classList.toggle('light')
-    //     back++
-    // }
 }
